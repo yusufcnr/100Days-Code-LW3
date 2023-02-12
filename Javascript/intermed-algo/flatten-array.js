af75997fc48354d009081d7c8f61d4c088064a07
yusufcnr <yusufcnr@gmail.com>
2023-02-12 16:36:19 +0300

    continue to intermed algo

--- Javascript/intermed-algo/flatten-array.js
+++ Javascript/intermed-algo/flatten-array.js
@@ -1,17 +1,16 @@
 //jshint esversion:6
 function steamrollArray(arr) {
-    let res=[];
+    let flatArray=[]; // new array to keep the track of elements
 
       for(let i = 0; i < arr.length; i++) {
-        if(Array.isArray(arr[i])) {
+        if(Array.isArray(arr[i])) { // check if the element is array
             
-            res.push(... steamrollArray(arr[i]));
+            flatArray.push(... steamrollArray(arr[i])); // call the function to flatten further until find a non-array element
         }
-        else res.push(arr[i]);
+        else flatArray.push(arr[i]); // push non-array items to final array
     }
 
-        return res;  
+        return flatArray;  
     }
-      
-       
+             
     console.log(steamrollArray([1, [2], [3, [[4]]]]));
