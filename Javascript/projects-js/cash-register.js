31dbab59add037ad4aada5c9e95b6333fcb829fb
yusufcnr <yusufcnr@gmail.com>
2023-02-22 13:45:04 +0300

    complete projects js

--- Javascript/projects-js/cash-register.js
+++ Javascript/projects-js/cash-register.js
@@ -1,15 +1,48 @@
 //jshint esversion:6
-
 function checkCashRegister(price, cash, cid) {
-
-
-
-
-    
-    let change;
-    return change;
-  }
+  const currencies = [
+    ["PENNY", 1],
+    ["NICKEL", 5],
+    ["DIME", 10],
+    ["QUARTER", 25],
+    ["ONE", 100],
+    ["FIVE", 500],
+    ["TEN", 1000],
+    ["TWENTY", 2000],
+    ["ONE HUNDRED", 10000]
+  ];
   
-  checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
-
-
+  let changeNeeded = (cash - price) * 100; // convert to cents
+  let changeAvailable = 0;
+  cid.forEach(item => changeAvailable += item[1] * 100); // convert to cents
+  
+  if (changeNeeded > changeAvailable) {
+    return {status: "INSUFFICIENT_FUNDS", change: []};
+  } else if (changeNeeded === changeAvailable) {
+    return {status: "CLOSED", change: cid};
+  } else {
+    let change = [];
+    for (let i = currencies.length - 1; i >= 0; i--) {
+      let currencyName = currencies[i][0];
+      let currencyValue = currencies[i][1];
+      let currencyAvailable = cid[i][1] * 100; // convert to cents
+      
+      if (changeNeeded >= currencyValue && currencyAvailable > 0) {
+        let numNeeded = Math.floor(changeNeeded / currencyValue);
+        let numAvailable = Math.floor(currencyAvailable / currencyValue);
+        let numToReturn = Math.min(numNeeded, numAvailable);
+        let amountToReturn = numToReturn * currencyValue;
+        
+        change.push([currencyName, amountToReturn / 100]); // convert back to dollars
+        
+        changeNeeded -= amountToReturn;
+      }
+    }
+    
+    if (changeNeeded > 0) {
+      return {status: "INSUFFICIENT_FUNDS", change: []};
+    } else {
+      return {status: "OPEN", change: change};
+    }
+  }
+}
