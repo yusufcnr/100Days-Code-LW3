d0fdc2a7bccc94f81d84161f35739ce10b2dc61d
yusufcnr <yusufcnr@gmail.com>
2023-03-01 16:11:04 +0300

    complete alchemy stacks

--- alchemy-univ-js/stacks.js
+++ alchemy-univ-js/stacks.js
@@ -1,34 +1,61 @@
-const { MAX_STACK_SIZE } = require('./config');
+//jshint esversion:6
+const { MAX_STACK_SIZE } = require("./config");
 
 class Stack {
-    constructor() {
-        this.items = [];
+  constructor() {
+    this.items = [];
+  }
+  push(item) {
+    if (this.items.length >= MAX_STACK_SIZE) {
+      throw new Error("stack overflow");
     }
-    push(item) {
-        
-        if(this.items.length >= MAX_STACK_SIZE) {
-            throw new Error ("stack overflow");
-        }
-        this.items.push(item);
+    this.items.push(item);
+  }
+  pop() {
+    if (this.items.length === 0) {
+      throw new Error("stack underflow");
     }
-    pop() {
-        if (this.items.length ===0) {
-             throw new Error ("stack underflow");
-        }        
-        return this.items.pop();
-        
+    return this.items.pop();
+  }
+  isEmpty() {
+    if (this.items.length === 0) {
+      return true;
     }
-    isEmpty() {
-        if (this.items.length === 0) {
-            return true;
-        } return false;
-        
-    }
-    peek() {
-        return this.items[this.items.length-1];
-
-        
-    }
+    return false;
+  }
+  peek() {
+    return this.items[this.items.length - 1];
+  }
 }
 
 module.exports = Stack;
+
+const Stack = require("./Stack");
+
+class OperationManager {
+  constructor() {
+    this.operations = new Stack();
+    this.undos = new Stack();
+  }
+
+  addOperation(operation) {
+    this.operations.push(operation);
+  }
+
+  undo() {
+    this.undos.push(this.operations.pop());
+  }
+
+  redo() {
+    this.operations.push(this.undos.pop());
+  }
+
+  redoAll() {
+      while (!this.undos.isEmpty()) {
+          this.redo();
+    }
+    }
+  }
+
+
+module.exports = OperationManager;
