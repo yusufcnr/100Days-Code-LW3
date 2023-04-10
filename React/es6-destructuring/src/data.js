3696694d305ba26e43120aaa1049f7bc86265cbb
yusufcnr <yusufcnr@gmail.com>
2023-04-11 00:06:59 +0300

    es6 destructuring practice

--- React/es6-destructuring/src/data.js
+++ React/es6-destructuring/src/data.js
@@ -1,6 +1,12 @@
 const animals = [
   { name: "cat", sound: "meow" },
-  { name: "dog", sound: "woof" }
+  {
+    name: "dog", sound: "woof",
+    feedingRequirements: {
+      food: 3,
+      water:2,
+    
+  }}
 ];
 
 export default animals;
--- React/es6-destructuring/src/index.js
+++ React/es6-destructuring/src/index.js
@@ -1,38 +1,38 @@
-import animals from "./data";
-
-
-
-console.log(animals);
-
-
-
-
-
-
-
-
-
-
 // CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice";
 
-// ReactDOM.render(
-//   <table>
-//     <tr>
-//       <th>Brand</th>
-//       <th>Top Speed</th>
-//     </tr>
-//     <tr>
-//       <td>{tesla.model}</td>
-//       <td>{teslaTopSpeed}</td>
-//       <td>{teslaTopColour}</td>
-//     </tr>
-//     <tr>
-//       <td>{honda.model}</td>
-//       <td>{hondaTopSpeed}</td>
-//       <td>{hondaTopColour}</td>
-//     </tr>
-//   </table>,
-//   document.getElementById("root")
-// );
+const [honda, tesla] = cars;
+
+const {
+  model: teslaModel,
+  coloursByPopularity: [teslaTopColour, teslaBottomColour],
+  speedStats: { topSpeed: teslaTopSpeed, teslaZeroToSixty },
+} = tesla;
+const {
+  model: hondaModel,
+  coloursByPopularity: [hondaTopColour, hondaBottomColour],
+  speedStats: { topSpeed: hondaTopSpeed, hondaZeroToSixty },
+} = honda;
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+      <th>Top Colour</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
