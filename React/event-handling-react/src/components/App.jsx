21a1c619674c405a71d0cf4c63faca2ba20ae2ff
yusufcnr <yusufcnr@gmail.com>
2023-04-12 00:24:28 +0300

    event handling in react start

---
+++ React/event-handling-react/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+function App() {
+
+
+  return (
+    <div className="container">
+      <h1>{headingText}</h1>
+      <input type="text" placeholder="What's your name?" />
+      <button >Submit</button>
+    </div>
+  );
+}
+
+export default App;
